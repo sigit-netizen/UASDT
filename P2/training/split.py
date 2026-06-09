99ad121034fcae91bbb54640ffb7bf787ca00284
MasRzkyy <sugipipit1@gmail.com>
2026-06-09 18:32:40 +0700

    pembuatan model LSTM dengan menggunakan dataset Cryptocurenncy Bitcoin

--- P2/training/split.py
+++ P2/training/split.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from pathlib import Path
 import os
 
 def main():
-    csv_path = '../dataset/air_pollution_china.csv'
-    output_dir = '../dataset/data'
+    BASE_DIR = Path(__file__).resolve().parent
+
+    csv_path = BASE_DIR.parent / 'dataset' / 'train.csv'
+    output_dir = BASE_DIR.parent / 'dataset' / 'data'
     
-    print("Membaca dataset air_pollution_china.csv...")
+    print("Membaca dataset train.csv...")
     df = pd.read_csv(csv_path)
     
     print("Membagi dataset menjadi 80% training dan 20% testing...")
